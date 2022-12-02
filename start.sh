b860a75b7c6b485c4eb7c090994c38a2179cda96
Pooja0033 <113719724+Pooja0033@users.noreply.github.com>
2022-12-02 23:36:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Pooja0033/Prv-DQ--Auto-filter-.git /Prv-DQ--Auto-filter-
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Prv-DQ--Auto-filter-
 fi
-cd /DQ-The-File-Donor
+cd /Prv-DQ--Auto-filter-
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
