d191ada9600c1bb78eb43b8405e048e3468d483e
Pooja0033 <113719724+Pooja0033@users.noreply.github.com>
2022-12-05 11:40:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Pooja0033/Rj-DQ--Auto-filter-.git /Prv-DQ--Auto-filter-
+  git clone https://github.com/Pooja0033/Rj-DQ--Auto-filter-.git /Rj-DQ--Auto-filter-
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Rj-DQ--Auto-filter-
 fi
 cd /Rj-DQ--Auto-filter-
 pip3 install -U -r requirements.txt
-echo "Starting Prv-DQ--Auto-filter-...."
+echo "Starting -DQ--Auto-filter-...."
 python3 bot.py
